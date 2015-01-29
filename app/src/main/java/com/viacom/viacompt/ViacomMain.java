27cf6e1df72dd31f853e8e1edadff026bcd33534
Shilpan Patel <shilpan@nyu.edu>
2015-01-29 13:30:43 -0500

    Initial commit

--- app/src/main/java/com/viacom/viacompt/ViacomMain.java
+++ app/src/main/java/com/viacom/viacompt/ViacomMain.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class VaicomMain extends ActionBarActivity {
+public class ViacomMain extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
